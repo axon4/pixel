07559101d13383d319a578f7d7d462d52d80fe86
axon4 <65050809+axon4@users.noreply.github.com>
2024-08-01 01:16:58 +0100

    create configuration package, add basic configuration & state

---
+++ configuration/configuration.go
@@ -0,0 +1,27 @@
+package configuration
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+)
+
+type BrushType = int
+
+type PixelCanvasConfiguration struct {
+	DrawingArea             fyne.Size
+	CanvasOffSet            fyne.Position
+	PixelRows, PixelColumns int
+	PixelSize               int
+}
+
+type State struct {
+	BrushColour    color.Color
+	BrushType      int
+	SwatchSelected int
+	FilePath       string
+}
+
+func (state *State) SetFilePath(path string) {
+	state.FilePath = path
+}
